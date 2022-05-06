a941ffd738ec4b1d066c19b7f88e74e62e4cc64f
Błażej Ryśnik <blazej.rysnik@gmail.com>
2022-05-06 21:49:57 +0200

    adding logging to testing

--- test/test_resources.py
+++ test/test_resources.py
@@ -1,9 +1,14 @@
 import re
+from logging import getLogger
 
 from scarab.web.resources import render_template
+
+
+logger = getLogger(__name__)
 
 
 def test_engine():
     template_str = render_template("status.html")
     assert type(template_str) is str
+    logger.info(f"Rendered html: {template_str}")
     assert re.findall(r"<html.*>", template_str)
